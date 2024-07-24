be835765350a072dcc5cbc90603fe6e81746aa01
Durgesh <durgesh3805@gmail.com>
2024-07-24 21:25:57 +0530

    Started new snake game

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-import pygame
-x=pygame.init()
-gameWindow = pygame.display.set_mode((1200,500))
+import pygame  #Importing pygame module
+x=pygame.init() #Init. pygame 
+gameWindow = pygame.display.set_mode((700,500)) 
+pygame.display.set_caption("Flappy Bird")
+
+
+
+exitGame= False
+gameOver= False
+
+# Creating game loop
+while not exitGame:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exitGame = True
+        if event.type ==pygame.KEYDOWN:
+            if event.key ==pygame.K_RIGHT:
+                print("Pressed right arrow key")
+
+
+
+        
+
+
+
+
+pygame.quit()
+print("Thank you for using")
+quit()
+
+
+
